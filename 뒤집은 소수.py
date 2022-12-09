05b8d61c66c3cfb323305f576fa3dfc0c13d0cc0
유현호 <nolj126@gmail.com>
2022-12-09 18:16:02 +0900

    https://velog.io/@rapid97/series/인프런-알고리즘

--- 뒤집은 소수.py
+++ 뒤집은 소수.py
@@ -1,6 +1,6 @@
-"https://velog.io/@rapid97/Python-인프런-알고리즘"
+"https://velog.io/@rapid97/Python-인프런-알고리즘"      <<<<<코드 설명은 여기서!!!!
 
-import sys<<<<<코드 설명은 여기서!!!!
+import sys
 sys.stdin = open("input.txt", "rt")
 
 """
